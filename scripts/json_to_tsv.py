c7a9b87ad16c7e1ff08b9f48cb3d2cae765a2ded
elnaske <naske.elias@outlook.com>
2026-02-26 13:31:27 -0500

    Add option to specify output dir with -o flag in json_to_tsv.py

--- scripts/json_to_tsv.py
+++ scripts/json_to_tsv.py
@@ -8,8 +8,8 @@ def get_parser():
     parser = argparse.ArgumentParser(
         description="Script for extracting JSON data from the SAP challenge 2 to TSV.")
     parser.add_argument("input", nargs='+', help="Input JSON file.")
-    parser.add_argument("-o", "--output", default=None,
-                        help="Output file name")
+    parser.add_argument("-o", "--output", nargs='+', default=None,
+                        help="Output file names or directory")
     return parser
 
 
@@ -18,11 +18,24 @@ def main():
     args = parser.parse_args()
 
     INFILES = args.input
+    OUTFILES = args.output
     ROOT = Path.cwd()
+    out_dir = ROOT
 
-    for INFILE in INFILES:
-        OUTFILE = args.output if args.output else INFILE.split('.')[0] + ".tsv"
+    if OUTFILES:
+        if not (len(OUTFILES) == len(INFILES) or len(OUTFILES) == 1):
+            raise ValueError(
+                "Number of input files must match number of output files, or output must be a directory")
+        if len(OUTFILES) == 1:
+            output = OUTFILES[0]
+            if output.endswith('/'):
+                out_dir = ROOT / output
+                out_dir.mkdir(parents=True, exist_ok=True)
 
+    if not OUTFILES or len(OUTFILES) <= 1:
+        OUTFILES = [out_dir / (f.split('.')[0] + ".tsv") for f in INFILES]
+
+    for OUTFILE, INFILE in zip(OUTFILES, INFILES):
         headings = ["path", "transcript", "etiology",
                     "prompt_category", "intelligibility"]
         with open(OUTFILE, 'w') as outfile:
