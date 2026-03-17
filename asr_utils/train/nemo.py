dc35f80a64dc114cde4852d5e14680ff2e4b7806
bglid <benjamin.h.glidden@gmail.com>
2026-03-17 14:38:47 -0400

    Adding checkpointing and saving to .nemo

--- asr_utils/train/nemo.py
+++ asr_utils/train/nemo.py
@@ -1,7 +1,9 @@
+from pathlib import Path
 import nemo.collections.asr as nemo_asr
 import lightning as L
 from peft import LoraConfig, get_peft_model
 from typing import Optional
+from lightning.pytorch.callbacks import ModelCheckpoint
 
 # Argument format references:
 # model_tag = "nvidia/parakeet-rnnt-0.6b"
@@ -32,7 +34,7 @@ def fine_tune_nemo_model(
     val_data_cfg: dict,
     optim_cfg: dict,
     trainer_kwargs: dict,
-    lora_cfg: Optional[LoraConfig] = None
+    lora_cfg: Optional[LoraConfig] = None,
 ):
     """Function for fine-tuning NeMo ASR models.
     Performs full fine-tuning by default.
@@ -43,9 +45,9 @@ def fine_tune_nemo_model(
         train_data_cfg (dict):  Dict containing training data configuration.
                                 Keys should contain at least "manifest_datapath", "sample_rate", and "batch_size".
         val_data_cfg (dict): Same as train_data_cfg, but for validation data.
-        optim_cfg (dict): Dict containing optimizer configuration, i.e. learning rate, optimizer, etc. 
+        optim_cfg (dict): Dict containing optimizer configuration, i.e. learning rate, optimizer, etc.
         trainer_kwargs (dict): Keyword arguments used when instantiating the Lightning trainer.
-        lora_cfg (LoraConfig, optional): LoRA configuration. If not None, all other layers will be frozen 
+        lora_cfg (LoraConfig, optional): LoRA configuration. If not None, all other layers will be frozen
                                          and only the adapters will be trained. Defaults to None.
     """
     asr_model = nemo_asr.models.ASRModel.from_pretrained(model_tag)
@@ -53,7 +55,21 @@ def fine_tune_nemo_model(
     asr_model.setup_validation_data(val_data_cfg)
     asr_model.setup_optimization(optim_cfg)  # TIP: increase LR
 
-    trainer = L.Trainer(**trainer_kwargs)
+    saving_tag = model_tag.replace("/", "_")
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_wer",
+        mode="min",
+        save_top_k=1,
+        save_last=True,
+        filename=saving_tag + "-{epoch:02d}-{val_wer:.4f}",
+    )
+
+    trainer = L.Trainer(callbacks=[checkpoint_callback], **trainer_kwargs)
+
+    # BHG - Adding .nemo file to load for inference
+    output = Path(trainer.default_root_dir)
+    output.mkdir(parents=True, exist_ok=True)
+    nemo_file = output / f"{saving_tag}.nemo"
 
     if lora_cfg:
         print("Using LoRA")
@@ -63,8 +79,16 @@ def fine_tune_nemo_model(
         # kind of hacky, but PeftModel doesn't work with Lightning
         lora_model.base_model.model.set_trainer(trainer)
         trainer.fit(lora_model.base_model.model)
+        lora_model.base_model.model.save_to(str(nemo_file))
     else:
         print("Full fine-tuning")
 
         asr_model.set_trainer(trainer)
         trainer.fit(asr_model)
+        asr_model.save_to(str(nemo_file))
+
+    print(f"Saved final .nemo model to {nemo_file}\n")
+
+    # doing the best ckpt too
+    best_ckpt = checkpoint_callback.best_model_path
+    print(f"Best checkpoint: {best_ckpt}\n")
