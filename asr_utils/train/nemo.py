da8b097afa5e8ee250b3be040888536d3a74fd68
bglid <benjamin.h.glidden@gmail.com>
2026-04-10 16:50:43 -0400

    adding datetime and second place to dump nemo models

--- asr_utils/train/nemo.py
+++ asr_utils/train/nemo.py
@@ -4,6 +4,7 @@ import lightning as L
 from peft import LoraConfig, get_peft_model
 from typing import Optional
 from lightning.pytorch.callbacks import ModelCheckpoint
+from datetime import datetime
 
 # Argument format references:
 # model_tag = "nvidia/parakeet-rnnt-0.6b"
@@ -66,10 +67,13 @@ def fine_tune_nemo_model(
 
     trainer = L.Trainer(callbacks=[checkpoint_callback], **trainer_kwargs)
 
-    # BHG - Adding .nemo file to load for inference
+    # trying both at once as a protective measure before committing to deleting the original
     output = Path(trainer.default_root_dir)
+    output2 = output / "nemo_models"
     output.mkdir(parents=True, exist_ok=True)
+    output2.mkdir(parents=True, exist_ok=True)
     nemo_file = output / f"{saving_tag}.nemo"
+    nemo_file2 = output2 / f"{datetime.today()}-{saving_tag}.nemo"
 
     if lora_cfg:
         print("Using LoRA")
@@ -86,8 +90,10 @@ def fine_tune_nemo_model(
         asr_model.set_trainer(trainer)
         trainer.fit(asr_model)
         asr_model.save_to(str(nemo_file))
+        asr_model.save_to(nemo_file2)
 
     print(f"Saved final .nemo model to {nemo_file}\n")
+    print(f"Saved final .nemo model to {nemo_file2} as well...\n")
 
     # doing the best ckpt too
     best_ckpt = checkpoint_callback.best_model_path
